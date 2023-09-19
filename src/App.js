e40a5469173706f07b2175aa87de4d48c1ea7c01
fdemarque <demarque.ads@gmail.com>
2023-09-18 23:35:37 -0300

    adicionando styled components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,138 @@
+// import React, { useState } from 'react';
+// import Message from './Message';
+// import Icone from './imagens/icon.png';
+
+// function ChatComponent() {
+//     const [messages, setMessages] = useState([]);
+//     const [userInput, setUserInput] = useState('');
+//     const [senderName, setSenderName] = useState('');
+
+//     const deleteMessage = (id) => {
+//       const updatedMessages = messages.filter((message, index) => index !== id);
+//       setMessages(updatedMessages);
+//     };
+  
+
+//     const addMessage = (sender, message, momento) => {
+//         setMessages([...messages, { sender, message, timestamp: momento}]);
+//         console.log (messages);
+//     };
+
+//     const handleSend = () => {
+//         const momento = new Date().toLocaleTimeString();
+//         if (userInput.trim() !== '') {
+//             addMessage(senderName, userInput, momento);
+//             if ((senderName === '')&&(senderName === 'Eu')){
+//                 setUserInput('');
+//             }else{
+//                 setSenderName('');
+//                 setUserInput('');
+//             };
+//         };
+//     };
+
+//     const handleInputChange = (event) => {
+//         setUserInput(event.target.value);
+//     };
+
+//     const handleInputKeyPress = (event) => {
+//         if (event.key === 'Enter') {
+//             handleSend();
+//         }
+//     };
+
+//     return (
+//         <div className="chat-container">
+//             <header className="header">
+//                 <img src={Icone} alt="Ícone" className="icon" />
+//                 <h3>UaiZAP</h3>
+//             </header>
+//             <div className="message-history">
+//               {messages.map((message, id) => (
+//                 <Message
+//                     key={id}
+//                     sender={message.sender}
+//                     message={message.message}
+//                     timeStamp={message.timestamp}
+//                     onDelete={() => deleteMessage(id)}
+//                     isLeft={message.sender !== ''}
+//                 />
+//               ))}
+//             </div>
+//             <input
+//                 type="text"
+//                 value={senderName}
+//                 onChange={(e) => setSenderName(e.target.value)}
+//                 placeholder="Seu Nome"
+//             />
+//             <input
+//                 type="text"
+//                 value={userInput}
+//                 onChange={handleInputChange}
+//                 onKeyDown={handleInputKeyPress}
+//                 placeholder="Digite sua mensagem"
+//             />
+//             <button onClick={handleSend}>Enviar</button>
+//         </div>
+//     );
+// }
+
+// export default ChatComponent;
+
 import React, { useState } from 'react';
-import './App.css';
+import styled from 'styled-components';
 import Message from './Message';
 import Icone from './imagens/icon.png';
-import Enviar from './imagens/trem.png';
+
+const ChatContainerWrapper = styled.div`
+    /* Estilos para o container geral */
+    background-color: #f0f0f0;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    padding: 20px;
+    max-width: 400px;
+    margin: 0 auto;
+`;
+
+const Header = styled.header`
+    /* Estilos para o cabeçalho */
+    text-align: center;
+
+    img {
+        width: 50px;
+        height: 50px;
+    }
+`;
+
+const MessageHistoryWrapper = styled.div`
+    /* Estilos para o histórico de mensagens */
+    margin-top: 20px;
+`;
+
+const InputWrapper = styled.div`
+    /* Estilos para o campo de entrada de texto e botão */
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 10px;
+
+    input[type='text'] {
+        flex: 1;
+        padding: 10px;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+        margin-right: 10px;
+    }
+
+    button {
+        background-color: #007bff;
+        color: #fff;
+        border: none;
+        border-radius: 5px;
+        padding: 10px 20px;
+        cursor: pointer;
+    }
+`;
 
 function ChatComponent() {
     const [messages, setMessages] = useState([]);
@@ -10,28 +140,25 @@ function ChatComponent() {
     const [senderName, setSenderName] = useState('');
 
     const deleteMessage = (id) => {
-      const updatedMessages = messages.filter((message, index) => index !== id);
-      setMessages(updatedMessages);
+        const updatedMessages = messages.filter((message, index) => index !== id);
+        setMessages(updatedMessages);
     };
-  
 
-    const addMessage = (sender, message) => {
-        const momento = new Date().toLocaleTimeString();
-        setMessages([...messages, { sender, message, timestamp: momento}]);
+    const addMessage = (sender, message, momento) => {
+        setMessages([...messages, { sender, message, timestamp: momento }]);
     };
 
     const handleSend = () => {
         const momento = new Date().toLocaleTimeString();
         if (userInput.trim() !== '') {
-            if (senderName ===''){
-                addMessage(senderName, userInput, momento);
+            addMessage(senderName, userInput, momento);
+            if ((senderName === '') && (senderName === 'Eu')) {
                 setUserInput('');
-            }else{
-                addMessage(senderName, userInput, momento);
+            } else {
                 setSenderName('');
                 setUserInput('');
-            };
-        };
+            }
+        }
     };
 
     const handleInputChange = (event) => {
@@ -45,40 +172,40 @@ function ChatComponent() {
     };
 
     return (
-        <div className="chat-container">
-            <header className="header">
-                <img src={Icone} alt="Ícone" className="icon" />
+        <ChatContainerWrapper>
+            <Header>
+                <img src={Icone} alt="Ícone" />
                 <h3>UaiZAP</h3>
-            </header>
-            <div className="message-history">
-              {messages.map((message, id) => (
-                <Message
-                    key={id}
-                    sender={message.sender}
-                    message={message.message}
-                    time={message.momento}
-                    onDelete={() => deleteMessage(id)}
-                    isLeft={message.sender !== ''}
+            </Header>
+            <MessageHistoryWrapper>
+                {messages.map((message, id) => (
+                    <Message
+                        key={id}
+                        sender={message.sender}
+                        message={message.message}
+                        timeStamp={message.timestamp}
+                        onDelete={() => deleteMessage(id)}
+                        isLeft={message.sender !== ''}
+                    />
+                ))}
+            </MessageHistoryWrapper>
+            <InputWrapper>
+                <input
+                    type="text"
+                    value={senderName}
+                    onChange={(e) => setSenderName(e.target.value)}
+                    placeholder="Seu Nome"
                 />
-              ))}
-            </div>
-            <input
-                type="text"
-                value={senderName}
-                onChange={(e) => setSenderName(e.target.value)}
-                placeholder="Seu Nome"
-            />
-            <input
-                type="text"
-                value={userInput}
-                onChange={handleInputChange}
-                onKeyDown={handleInputKeyPress}
-                placeholder="Digite sua mensagem"
-            />
-            <button onClick={handleSend}>
-                <img src={Enviar} alt="Enviar" className="botao-de-envio" />
-            </button>
-        </div>
+                <input
+                    type="text"
+                    value={userInput}
+                    onChange={handleInputChange}
+                    onKeyDown={handleInputKeyPress}
+                    placeholder="Digite sua mensagem"
+                />
+                <button onClick={handleSend}>Enviar</button>
+            </InputWrapper>
+        </ChatContainerWrapper>
     );
 }
 
--- src/Message.js
+++ src/Message.js
@@ -1,43 +1,35 @@
-// import React from 'react';
-
-// function Message({ sender, message, momentoFormatado, onDelete }) {
-//     const isSenderEmpty = sender.trim() === '';
-//     const messageStyle = {
-//         padding: '5px',
-//         borderRadius: '5px',
-//         margin: '5px',
-//         cursor: 'pointer',
-//         textAlign: isSenderEmpty ? 'right' : 'left',
-//     };
-//     if (sender !== ""){
-//         return (
-//             <div
-//                 onDoubleClick={onDelete}
-//                 style={messageStyle}
-//                 className={isSenderEmpty ? 'right-message' : 'left-message'}
-//             >
-//                 <strong>{sender}</strong><p/>{message}<p/>{momentoFormatado}
-//             </div>
-//         );    
-//     }else{
-//         return (
-//             <div
-//                 onDoubleClick={onDelete}
-//                 style={messageStyle}
-//                 className={isSenderEmpty ? 'right-message' : 'left-message'}
-//             >
-//                 {message}<p/>{momentoFormatado}
-//             </div>
-//         ); 
-//     }
-// }
-
-// export default Message;
-
 import React from 'react';
+import styled from 'styled-components';
 
-function Message({ sender, message, momentoFormatado, onDelete }) {
-    const isSenderEmpty = sender.trim() === '';
+const MessageListContainer = styled.div`
+    max-height: 400px; /* Altura máxima para habilitar a rolagem */
+    overflow-y: auto; /* Adiciona rolagem vertical quando necessário */
+`;
+
+const MessageContainer = styled.div`
+    background-color: ${(props) => (props.isSenderEmpty ? '#007bff' : '#f0f0f0')};
+    color: ${(props) => (props.isSenderEmpty ? '#fff' : '#000')};
+    padding: 10px;
+    border-radius: 10px;
+    margin: 5px;
+    max-width: 70%;
+    word-wrap: break-word;
+    align-self: ${(props) => (props.isSenderEmpty ? 'flex-end' : 'flex-start')};
+`;
+
+const SenderName = styled.strong`
+    display: block;
+    margin-bottom: 5px;
+`;
+
+const TimeStamp = styled.h6`
+    font-size: 12px;
+    text-align: right;
+    color: #777;
+`;
+
+function Message({ sender, message, timeStamp, onDelete }) {
+    const isSenderEmpty = ((sender.trim() === '')||(sender.trim() === 'Eu')||(sender.trim() === 'eu'));
     const messageStyle = {
         padding: '5px',
         borderRadius: '5px',
@@ -47,17 +39,32 @@ function Message({ sender, message, momentoFormatado, onDelete }) {
         class: isSenderEmpty ? 'right-message' : 'left-message',
     };
 
+    if ((sender.trim() === "")||(sender.trim() === "Eu")||(sender.trim() === "eu")){
+        return (
+            <div
+                onDoubleClick={onDelete}
+                style={messageStyle}
+                className={messageStyle.class}
+            >
+                <strong>Você</strong>
+                <p>{message}</p>
+                <h6>{timeStamp}</h6>
+            </div>
+        );
+    }else{
+        return (
+            <div
+                onDoubleClick={onDelete}
+                style={messageStyle}
+                className={messageStyle.class}
+            >
+                <strong>{sender}</strong>
+                <p>{message}</p>
+                <h6>{timeStamp}</h6>
+            </div>
+        );
+    }
 
-    return (
-        <div
-            onDoubleClick={onDelete}
-            style={messageStyle}
-            className={messageStyle.class}
-        >
-            <strong>{sender}</strong>
-            <p>{message}</p>{momentoFormatado}
-        </div>
-    );
 }
 
 export default Message;
