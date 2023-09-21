49bc760f01bb39ff23f0c0da584b216878ed22be
fdemarque <demarque.ads@gmail.com>
2023-09-21 05:42:05 -0300

    finalizado

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import icone from './imagens/icon.png';
 import background from './imagens/background.png';
 import trem from './imagens/trem.png';
 
+// estilização da div pai
 const ChatContainerWrapper = styled.div`
     /* Estilos para o container geral */
     background: url(${background}) fixed;
@@ -18,6 +19,7 @@ const ChatContainerWrapper = styled.div`
     overflow: hidden;
 `;
 
+// estilização do cabeçalho
 const Header = styled.header`
     /* Estilos para o cabeçalho */
     text-align: center;
@@ -35,6 +37,7 @@ const Header = styled.header`
     }
 `;
 
+// estilização do histórico de mensagens
 const MessageHistoryWrapper = styled.div`
     /* Estilos para o histórico de mensagens */
     margin-top: 20px;
@@ -43,6 +46,7 @@ const MessageHistoryWrapper = styled.div`
     z-index: 2;  
 `;
 
+// estilização dos componentes referentes ao envio de mensagens
 const InputWrapper = styled.div`
     /* Estilos para o campo de entrada de texto e botão */
     position: fixed;
@@ -64,7 +68,7 @@ const InputWrapper = styled.div`
         border-radius: 5px;
         margin-right: 10px;
     }
-
+    
     button {
         background-image: url(${trem});
         background-size: 30px 30px;
@@ -74,28 +78,32 @@ const InputWrapper = styled.div`
         color: #fff;
         border: none;
         border-radius: 5px;
-        width: 30px; 
+        width: 10px; 
         height: 30px; 
         padding: 20px;
         cursor: pointer;
     }    
 `;
 
-
+// Componente principal do chat
 function ChatComponent() {
+    // Estados para armazenar mensagens, entrada do usuário e nome do remetente
     const [messages, setMessages] = useState([]);
     const [userInput, setUserInput] = useState('');
     const [senderName, setSenderName] = useState('');
 
+    // Função para excluir uma mensagem com base no índice
     const deleteMessage = (id) => {
         const updatedMessages = messages.filter((message, index) => index !== id);
         setMessages(updatedMessages);
     };
 
+    // Função para adicionar uma nova mensagem ao histórico
     const addMessage = (sender, message, momento) => {
         setMessages([...messages, { sender, message, timestamp: momento }]);
     };
 
+    // Função para lidar com o envio de uma mensagem
     const handleSend = () => {
         const momento = new Date().toLocaleTimeString();
         if (userInput.trim() !== '') {
@@ -109,21 +117,26 @@ function ChatComponent() {
         }
     };
 
+    // Função para atualizar o estado da entrada do usuário
     const handleInputChange = (event) => {
         setUserInput(event.target.value);
     };
 
+    // Função para lidar com o pressionamento da tecla "Enter" para enviar mensagem
     const handleInputKeyPress = (event) => {
         if (event.key === 'Enter') {
             handleSend();
         }
     };
 
+    // Renderiza a interface do chat
     return (
         <ChatContainerWrapper>
+            {/* Cabeçalho com ícone */}
             <Header>
                 <img src={icone} alt="Ícone"/>
             </Header>
+            {/* Histórico de mensagens */}
             <MessageHistoryWrapper>
                 {messages.map((message, id) => (
                     <Message
@@ -136,13 +149,16 @@ function ChatComponent() {
                     />
                 ))}
             </MessageHistoryWrapper>
+            {/* Campo de entrada de texto e botão */}
             <InputWrapper>
+                {/* Input para o nome do remetente */}
                 <input
                     type="text"
                     value={senderName}
                     onChange={(e) => setSenderName(e.target.value)}
                     placeholder="Seu Nome"
                 />
+                {/* Input para a mensagem do usuário */}
                 <input
                     type="text"
                     value={userInput}
@@ -150,10 +166,12 @@ function ChatComponent() {
                     onKeyDown={handleInputKeyPress}
                     placeholder="Digite sua mensagem"
                 />
+                {/* Botão para enviar mensagem */}
                 <button onClick={handleSend}></button>
             </InputWrapper>
         </ChatContainerWrapper>
     );
 }
 
+// Exporta o componente ChatComponent para uso em outros lugares
 export default ChatComponent;
--- src/Message.js
+++ src/Message.js
@@ -1,52 +1,63 @@
 import React from 'react';
 import styled from 'styled-components';
 
+// Define um contêiner para a lista de mensagens com uma altura máxima e rolagem vertical.
 const MessageListContainer = styled.div`
-    max-height: 400px; 
-    overflow-y: auto; 
+    max-height: 400px;
+    overflow-y: auto;
 `;
+
+// Define um parágrafo estilizado.
 const P = styled.p`
-    margin:0;
-`
+    margin: 0;
+`;
+
+// Define o contêiner para cada mensagem.
 const MessageContainer = styled.div`
     background-color: ${(props) => (props.isSenderEmpty ? '#ff0010' : '#f0f0f0')};
     color: ${(props) => (props.isSenderEmpty ? '#fff' : '#000')};
     padding: 10px;
     border-radius: 10px;
     max-width: 30%;
-    height:10%;
+    height: 10%;
     word-wrap: break-word;
     margin: ${(props) => (props.isSenderEmpty ? '0 0 5px 70%' : '0 0 5px 0')};
-    border: 2px solid #000; 
-
+    border: 2px solid #000;
+    cursor: pointer;
 `;
 
+// Define o nome do remetente em negrito.
 const SenderName = styled.strong`
     display: block;
-    
 `;
 
+// Define a adição do momento de envio à mensagem.
 const TimeStamp = styled.h6`
-    font-size: 12px;
-    text-align: left;
-    color: #777;
-    margin:0;
+    font-size: 9px;
+    text-align: right;
+    margin: 0;
 `;
 
 function Message({ sender, message, timeStamp, onDelete }) {
+    // Verifica se o nome do remetente está vazio ou igual a 'eu'.
     const isSenderEmpty = sender.trim().toLowerCase() === '' || sender.trim().toLowerCase() === 'eu';
 
     return (
         <MessageListContainer>
+            {/* Renderiza o contêiner da mensagem com base no remetente. */}
             <MessageContainer isSenderEmpty={isSenderEmpty} onDoubleClick={onDelete}>
                 {!isSenderEmpty ? (
+                    // Renderiza o nome do remetente se não estiver vazio.
                     <SenderName>{sender}</SenderName>
                 ) : (
+                    // Caso contrário, renderiza 'Você'.
                     <SenderName>
                         <strong>Você</strong>
                     </SenderName>
                 )}
+                {/* Renderiza o conteúdo da mensagem */}
                 <P>{message}</P>
+                {/* Renderiza o carimbo de data e hora da mensagem. */}
                 <TimeStamp>{timeStamp}</TimeStamp>
             </MessageContainer>
         </MessageListContainer>
